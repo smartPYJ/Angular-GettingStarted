ef8179112fad062a325b1cec8428c9fa7dd140a6
smarpPYJ <uchemonday01@gmail.com>
2021-11-14 13:09:10 +0100

    worked of hide image

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { productListComponent } from './products/product-list.component';
@@ -11,7 +12,9 @@ import { productListComponent } from './products/product-list.component';
   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   bootstrap: [AppComponent]
 })
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -10,10 +10,12 @@ import { Component } from "@angular/core";
 export class productListComponent {
 
   pageTitle: string = 'Product Title ';
-  imageWidth : number =50;
-  imageMargin : number = 2;
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = 'Cart';
 
-  products: any [] =[
+  products: any[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
@@ -25,47 +27,48 @@ export class productListComponent {
       "imageUrl": "assets/images/leaf_rake.png"
     },
     {
-  "productId": 2,
-    "productName": "Garden Cart",
+      "productId": 2,
+      "productName": "Garden Cart",
       "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2021",
-          "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-              "starRating": 4.2,
-                "imageUrl": "assets/images/garden_cart.png"
-},
-{
-  "productId": 5,
-    "productName": "Hammer",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
       "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2021",
-          "description": "Curved claw steel hammer",
-            "price": 8.9,
-              "starRating": 4.8,
-                "imageUrl": "assets/images/hammer.png"
-},
-{
-  "productId": 8,
-    "productName": "Saw",
+      "releaseDate": "May 21, 2021",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    },
+    {
+      "productId": 8,
+      "productName": "Saw",
       "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2021",
-          "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-              "starRating": 3.7,
-                "imageUrl": "assets/images/saw.png"
-},
-{
-  "productId": 10,
-    "productName": "Video Game Controller",
+      "releaseDate": "May 15, 2021",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "assets/images/saw.png"
+    },
+    {
+      "productId": 10,
+      "productName": "Video Game Controller",
       "productCode": "GMG-0042",
-        "releaseDate": "October 15, 2020",
-          "description": "Standard two-button video game controller",
-            "price": 35.95,
-              "starRating": 4.6,
-                "imageUrl": "assets/images/xbox-controller.png"
-}
+      "releaseDate": "October 15, 2020",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "assets/images/xbox-controller.png"
+    }
   ];
+  toggleImage(): void {
+    this.showImage = !this.showImage;
 
-
-
+  }
 }
